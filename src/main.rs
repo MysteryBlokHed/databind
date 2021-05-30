31fd4db008109494cba47dcd6bf26fad1b068217
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-05-30 14:28:42 -0400

    Create CLI and add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
+use clap::{App, Arg};
+use std::fs;
+
 mod settings;
 mod token;
 mod transpiler;
 
 fn main() {
+    let matches = App::new("Databind")
+        .version("0.1.0")
+        .author("Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>")
+        .about("A superset of mcfunctions for Minecraft Datapacks.")
+        .arg(
+            Arg::with_name("FILE")
+                .help("The databind file to transpile")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("random-var-names")
+                .long("random-var-names")
+                .help("Add characters to the end of variable names"),
+        )
+        .arg(
+            Arg::with_name("var-display-names")
+                .long("var-display-names")
+                .help(
+                    "Change the display name of variables in-game to hide extra characters. \
+                    Only relevant with --random-var-names",
+                ),
+        )
+        .get_matches();
+
     let transpiler_settings = settings::Settings {
-        randomize_var_names: true,
-        var_display_name: true,
+        randomize_var_names: matches.is_present("random-var-names"),
+        var_display_name: matches.is_present("var-display-names"),
     };
 
-    let totally_real_file = String::from(
-        ":var example .= 1\r\n\
-:var newVar .= 3\r\n\
-:var example += 5\r\n\
-:var example = 3\r\n\
-:var newVar -= 2\r\n\
-# Test if things worked
-execute if :tvar newVar matches 1 run say newVar subtraction worked\r\n\
-execute if :tvar example matches 3 run say example setting worked\r\n",
-    );
+    let file = matches.value_of("FILE").unwrap();
+    let content = fs::read_to_string(file).unwrap();
 
-    let mut transpile = transpiler::Transpiler::new(totally_real_file, transpiler_settings);
+    let mut transpile = transpiler::Transpiler::new(content, transpiler_settings);
     let tokens = transpile.tokenize();
     println!("{:?}", tokens);
 
