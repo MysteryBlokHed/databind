e9fd9fdc072e39d2a012270631d9f1c4bbc25f93
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-06-25 21:22:23 -0400

    Fix negative integers not working

--- src/compiler/tokenize.rs
+++ src/compiler/tokenize.rs
@@ -18,7 +18,7 @@
 use super::Compiler;
 use crate::token::Token;
 
-const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const DIGITS: [char; 11] = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 impl Compiler<'_> {
     /// Convert the provided file contents into a list of tokens
