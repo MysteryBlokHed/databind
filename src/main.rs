c7f80003737b4241223dc5c1741330f2e77cca90
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-07-26 20:11:16 -0400

    Revert test commits

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-// Test 1
-// Test 2
 #![warn(clippy::all)]
 
 use glob::glob;
