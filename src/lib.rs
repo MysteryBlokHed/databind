d746a75b8fa82eaa7fb092d33595f3dc6b90af32
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-08-29 20:15:42 -0400

    Update quick start code in lib.rs
    Removes a leftover test function joe() from the snippet in lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -32,29 +32,25 @@
 //! use databind::compiler::Compiler;
 //! use std::collections::HashMap;
 //!
-//! #[test]
-//! fn joe() {
-//!     // Source file
-//!     let source_file = "
-//!     func main\n\
-//!         say Hello, World!\n\
-//!     end\n\
-//!     func second_func\n\
-//!         say Second function\n\
-//!     end"
-//!     .to_string();
-//!
-//!     // Create compiler and tokenize source file
-//!     let mut compiler = Compiler::new(source_file, None);
-//!     let tokens = compiler.tokenize();
-//!     // Compile tokens to files
-//!     let compiled = compiler.compile(tokens, None, "", &HashMap::new(), false);
-//!     // Print the contents of each file
-//!     for (k, v) in compiled.filename_map.iter() {
-//!         println!("Output File: {}.mcfunction", k);
-//!         println!("Contents:\n{}", compiled.file_contents[*v]);
-//!         println!("----------");
-//!     }
+//! // Source file
+//! let source_file = "
+//! func main\n\
+//!     say Hello, World!\n\
+//! end\n\
+//! func second_func\n\
+//!     say Second function\n\
+//! end"
+//! .to_string();
+//! // Create compiler and tokenize source file
+//! let mut compiler = Compiler::new(source_file, None);
+//! let tokens = compiler.tokenize();
+//! // Compile tokens to files
+//! let compiled = compiler.compile(tokens, None, "", &HashMap::new(), false);
+//! // Print the contents of each file
+//! for (k, v) in compiled.filename_map.iter() {
+//!     println!("Output File: {}.mcfunction", k);
+//!     println!("Contents:\n{}", compiled.file_contents[*v]);
+//!     println!("----------");
 //! }
 //! ```
 //!
