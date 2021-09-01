41f0b172383976207d53e45e67ab47cdbb7ab625
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-08-31 21:23:14 -0400

    Revert "Trigger workflows"
    This reverts commit bbaa05964488ed6d17826fff8e5a9de97798079e.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-// Trigger workflows
 #![warn(clippy::all)]
 
 use same_file::is_same_file;
