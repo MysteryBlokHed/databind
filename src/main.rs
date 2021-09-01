bbaa05964488ed6d17826fff8e5a9de97798079e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-08-31 21:22:32 -0400

    Trigger workflows
    Adds a comment to main.rs to make the Build And Test workflows run. Will
    be reverted.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+// Trigger workflows
 #![warn(clippy::all)]
 
 use same_file::is_same_file;
